a0ae5e390e0fff8e5c509cf8e699abc775ae3bb4
Roberto Bastone <35118511+robertobastone@users.noreply.github.com>
2018-10-30 13:06:19 +0100

    Add files via upload

---
+++ PrimeHunter_1.0.py
@@ -0,0 +1,18 @@
+######################### Prime Hunter
+
+# author: Roberto Bastone
+# email: robertobastone93@gmail.com
+
+
+# code
+
+print "Welcome to the prime Hunter"
+
+num = input("Type an integer, the hunter will tell whether it is a prime number.\n")
+div = 2
+while ( int(num) % div != 0):
+	div = div + 1
+if ( int(num) == div):
+	print "The Hunter says that", num, "is prime"
+else:
+	print "The Hunter says that", div, "is the divisor of", num
